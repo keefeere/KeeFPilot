b74fbeb534cb7d7ef8f3eea07a9c0b977801ffbf
keefeere <keefeere@ukr.net>
2025-09-23 11:46:03 +0300

    new test

---
+++ frogpilot/ui/translation_anchor.cc
@@ -0,0 +1,10 @@
+// translation_anchor.cc
+// stub for proper lupdate work
+static const char *unused_translations[] = {
+  QT_TRANSLATE_NOOP("DriveStats", "ALL TIME"),
+  QT_TRANSLATE_NOOP("DriveStats", "ALL TIME (KONIK)"),
+  QT_TRANSLATE_NOOP("DriveStats", "PAST WEEK (KONIK)"),
+  QT_TRANSLATE_NOOP("DriveStats", "PAST WEEK (KONIK)"),
+  QT_TRANSLATE_NOOP("FrogPilotThemesPanel", "Human-tested, frog-approved 🐸"),
+  QT_TRANSLATE_NOOP("FrogPilotThemesPanel", "Hop in and buckle up!"),
+};
