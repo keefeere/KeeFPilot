de3b92150a4a2d2bc28f0dfc88ab0192824df749
keefeere <keefeere@ukr.net>
2025-09-24 10:27:51 +0300

    test 2

--- selfdrive/ui/qt/onroad/alert_tr.h
+++ selfdrive/ui/qt/onroad/alert_tr.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <QString>
-#include <QMap>
-#include <QObject>
 #include <QStringList>
 #include <map>
 
@@ -10,17 +8,35 @@ struct AlertTranslation {
     QString text2;
 };
 
-extern std::map<QString, AlertTranslation> alertTranslations;
 
 // обʼяви функції прямо тут
 inline QString translateAlertText1(const QString &text, const QStringList &params = {});
 inline QString translateAlertText2(const QString &text, const QStringList &params = {});
 
+inline QString translateAlertText1(const QString &text, const QStringList &params) {
+    auto it = std::find_if(alertTranslations.begin(), alertTranslations.end(),
+                           [&text](const auto &pair){ return pair.text1 == text; });
 
+    if (it == alertTranslations.end()) return text;
 
+    QString translated = QObject::tr(it->text1.toUtf8().constData());
+    for (int i = 0; i < params.size(); ++i) translated = translated.arg(params[i]);
+    return translated;
+}
+
+inline QString translateAlertText2(const QString &text, const QStringList &params) {
+    auto it = std::find_if(alertTranslations.begin(), alertTranslations.end(),
+                           [&text](const auto &pair){ return pair.text2 == text; });
+
+    if (it == alertTranslations.end()) return text;
+
+    QString translated = QObject::tr(it->text2.toUtf8().constData());
+    for (int i = 0; i < params.size(); ++i) translated = translated.arg(params[i]);
+    return translated;
+}
 inline QMap<QString, AlertTranslation> alertTranslations = {
-  {"CalibrationInProgress", {QObject::tr("Calibration in Progress: %1%"), QObject::tr("Drive Above %1 km/h")}},
-  {"SystemOverheated",      {QObject::tr("System Overheated"),            QObject::tr("%1 °C")}},
-  {"StartupWarning",        {QObject::tr("WARNING: This branch is not tested"), QObject::tr("%1")}},
-  {"StartupMessage",        {QObject::tr("Hop in and buckle up!"),        QObject::tr("Human-tested, frog-approved 🐸")}}
+  {"CalibrationInProgress", {QT_TR_NOOP("Calibration in Progress: %1%"), QT_TR_NOOP("Drive Above %1 km/h")}},
+  {"SystemOverheated",      {QT_TR_NOOP("System Overheated"),            QT_TR_NOOP("%1 °C")}},
+  {"StartupWarning",        {QT_TR_NOOP("WARNING: This branch is not tested"), QT_TR_NOOP("%1")}},
+  {"StartupMessage",        {QT_TR_NOOP("Hop in and buckle up!"),        QT_TR_NOOP("Human-tested, frog-approved 🐸")}}
 };
